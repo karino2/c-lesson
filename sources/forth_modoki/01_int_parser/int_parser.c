7a8f92459d68b022eb74f76bd53320187f10d28a
Kazuma Arino <hogeika2@gmail.com>
2018-09-17 12:43:04 +0900

    Int parser question.

---
+++ sources/forth_modoki/01_int_parser/int_parser.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <assert.h>
+
+static const char* input = "123 456";
+
+
+int main() {
+    int answer1 = 0;
+    int answer2 = 0;
+
+    // write something here.
+
+
+    // verity result.
+    assert(answer1 == 123);
+    assert(answer2 == 456);
+
+    return 1;
+}
