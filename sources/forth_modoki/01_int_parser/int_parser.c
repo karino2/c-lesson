68821cc7912a7a78800859d32dd6687a53f14176
devel <oiku1515@gmail.com>
2018-09-19 11:45:03 +0900

    ・inputの型，値を変更 ・input文字列のコピーを廃止(my_inputの削除) ・文字列の長さを数える関数を追加 ・文字列を分割するときに部分文字列の長さを数えるように変更 ・文字と数字を変換する時に文字列の先頭のポインタと文字列の長さを用いるように変更 ・コメントの修正

--- sources/forth_modoki/01_int_parser/int_parser.c
+++ sources/forth_modoki/01_int_parser/int_parser.c
@@ -1,62 +1,66 @@
 #include <stdio.h>
 #include <assert.h>
 
-static const char* input = "123 456";
+static const char* const input = "1203 456";
 
-// 文字列のコピー
-void _strcpy(char *s, char *t){
-    int i = 0;
 
-    while((s[i] = t[i]) != '\0') // \0までポインタ同士を代入していく
-        i++;
+int _strlen(char *p)
+{
+    int len=0;
+
+    while(p[len] != '\0' && p[len] != ' '){
+        len++;
+    }
+    return len;
 }
 
 
-# 文字列の分割
-int split(char *dst[], int*dst_len, char *src, char delim){
+// 文字列の分割と部分文字列の長さを数える
+int split(char **dst, int *dst_len, char *src){
     int count = 0;
 
     while(1) {
-        while (*src == delim){
+        while (*src == ' '){
             src++;
         }
 
         if (*src == '\0') break;
 
+        dst_len[count] = _strlen(src);
         dst[count++] = src;
-        while (*src && *src != delim) {
+
+        while (*src && *src != ' ') {
             src++;
         }
 
         if (*src == '\0') break;
 
-        *src++ = '\0';
     }
     return count;
 }
 
 
-# 文字と数字の変換
+// 数字かどうかの判定
 int _isdigit(char c)
 {
-    int num;
 
     if (c >= '0' && c <= '9'){
-        num = c - '0';
+        return 1;
     }else{
-        num = 0;
+        return 0;
     }
-    return num;
 }
 
-# 複数桁の文字を数字の変換
-int _atoi(char s[])
+// 複数桁の文字を数字の変換
+int _atoi(char *s, int len)
 {
     int i, n;
     i = 0;
 
-    for(n=0; _isdigit(s[i]); i++){
-        n = 10 * n + (s[i] - '0');
+    for(n=0; i <= len; i++){
+        if(_isdigit(s[i])) {
+            n = 10 * n + (s[i] - '0');
+        }
     }
     return n;
 }
@@ -72,15 +76,13 @@ int main() {
     int count;
     int my_len_arr[100];
 
+
+    count = split(my_input_array, &my_len_arr, input);
+
+
     int numbers[100];
-
-    char my_input[100];
-    _strcpy(my_input, input);
-
-    count = split(my_input_array, my_len_arr, my_input, ' ');
-
     for (int i = 0; i < count; ++i) {
-        numbers[i] = _atoi(my_input_array[i]);
+        numbers[i] = _atoi(my_input_array[i], my_len_arr[i]);
     }
 
     answer1 = numbers[0];
@@ -88,7 +90,7 @@ int main() {
 
 
     // verity result.
-    assert(answer1 == 123);
+    assert(answer1 == 1203);
     assert(answer2 == 456);
 
     return 1;
