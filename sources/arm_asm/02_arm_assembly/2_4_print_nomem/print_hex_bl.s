647ff4e4394863bf138a74a0a52f38ec91c18a18
devel <oiku1515@gmail.com>
2018-11-23 13:48:09 +0900

    [add]　print_hex_bl.sの実装

--- sources/arm_asm/02_arm_assembly/2_4_print_nomem/print_hex_bl.s
+++ sources/arm_asm/02_arm_assembly/2_4_print_nomem/print_hex_bl.s
@@ -1,15 +1,36 @@
 .globl _start
 _start:
+	ldr r0,=0x101f1000
 
-  mov r0, r15
-  bl print_hex
+	mov r1, r15
+	mov r3, #28
+	bl print_hex
 
-  mov r0, #0x68
-  bl print_hex
+	mov r1, #0x68
+	mov r3, #28
+	bl print_hex
 
 busy:
   b busy
 
 
+# レジスタの値を16進数で表示する
 print_hex:
-  // TODO: write here!
+    lsr r2, r1, r3
+    and r2, r2, #0x0f
+    cmp r2, #0x0a
+    blt over_ten
+    add r2, r2, #0x07
+
+over_ten:
+    add r2, r2, #0x30
+    str r2, [r0]
+
+    sub r3, r3, #4
+
+    cmp r3, #0
+    bge print_hex
+    mov r15, r14
+
+# >>0001000C00000068
+# 改行されていないので、0001000C と00000068 が出力
