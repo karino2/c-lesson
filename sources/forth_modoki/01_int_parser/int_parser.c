c994a2d0ce6610027a0ab6c43d8bf68fd21b35e2
devel <oiku1515@gmail.com>
2018-11-25 12:42:55 +0900

    [fix] int_parser.cの追加

---
+++ sources/forth_modoki/01_int_parser/int_parser.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <assert.h>
+
+static const char* const input = "123 456  1203";
+
+
+int main() {
+    int answer1 = 0;
+    int answer2 = 0;
+    int answer3 = 0;
+
+    // write something here.
+
+
+    // verity result.
+    assert(answer1 == 123);
+    assert(answer2 == 456);
+    assert(answer3 == 1203);
+
+    return 1;
+}
