6f0a29ad940431478f1abd77d7435351edf317d9
Kazuma Arino <hogeika2@gmail.com>
2019-03-15 12:54:35 +0900

    Add seperate compilation explanation.

--- sources/arm_asm/06_c_function/sep_comp/hello_printf.c
+++ sources/arm_asm/06_c_function/sep_comp/hello_printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+void func1(char *str) {
+    puts(str);
+}
+
 void print_something(char *str) {
-    printf(str);
+    func1(str);
 }
 
--- sources/arm_asm/06_c_function/sep_comp/main.c
+++ sources/arm_asm/06_c_function/sep_comp/main.c
@@ -1,7 +1,7 @@
 void print_something(char *str);
 
 int main() {
-    print_something("Hello World\n");
+    print_something("Hello World");
     return 0;
 }
 
