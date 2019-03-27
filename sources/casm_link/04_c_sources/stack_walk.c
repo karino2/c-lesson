ec5cea4ad94ae4593bc8ba850b9b3af755e23167
Kazuma Arino <hogeika2@gmail.com>
2019-03-27 21:09:56 +0900

    Add stack_walk.c.

---
+++ sources/casm_link/04_c_sources/stack_walk.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int func3 (int a4) {
+    return a4*3;
+}
+
+int func2(int a2) {
+    int a3 = 0;
+    for(int i = 0; i < 10; i++) {
+        a3+=i;
+    }
+    return func3(a2+a3)+2;
+}
+
+int func1(int a1) {
+    return func2(a1)+1;
+}
+
+
+int main() {
+    int res = func1(123);
+    printf("result is %d\n", res);
+    return 0;
+}
+
